60870a5f594dcf1d44d480192a4fd9dc3cac56f7
Morgen <flyingpig86@yahoo.com>
2019-04-25 17:46:43 -0400

    Big PUSH

---
+++ WebApplication1/Controllers/RestaurantController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult about()
+        {
+
+            return View();
+        }
+
+        public ActionResult gallery()
+        {
+
+            return View();
+        }
+
+        public ActionResult Guide()
+        {
+
+            return View();
+        }
+
+        public ActionResult menu()
+        {
+
+            return View();
+        }
+    }
+}
